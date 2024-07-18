c8fc6d280ab5f4380e27d0838712d1e172522dba
Lee-KyungSeok <leeseok8347@gmail.com>
2024-07-18 21:58:16 +0900

    LineCommanderTest 에 존재하던 비즈니스 로직을 LineTest 로 변경

--- src/test/java/subway/fixtures/LineFixture.java
+++ src/test/java/subway/fixtures/LineFixture.java
@@ -2,6 +2,7 @@ package subway.fixtures;
 
 import subway.domain.command.LineCommand;
 import subway.domain.entity.line.Line;
+import subway.domain.entity.line.LineSections;
 
 import java.util.UUID;
 
@@ -10,7 +11,11 @@ public class LineFixture {
         return Line.init(new LineCommand.CreateLine(UUID.randomUUID().toString(), UUID.randomUUID().toString(), upStationId, downStationId, 10L));
     }
 
-    public static Line prepareLineOne(Long upStationId, Long downStationId) {
-        return Line.init(new LineCommand.CreateLine("1호선", "#0052A4", upStationId, downStationId, 10L));
+    public static Line prepareLineOne(Long firstStationId, Long lastStationId) {
+        Line line = new Line("1호선", "#0052A4", new LineSections());
+        for (long i = firstStationId; i<lastStationId; i++) {
+            line.addSection(i, i+1, 10L);
+        }
+        return line;
     }
 }
--- src/test/java/subway/unit/entity/LineTest.java
+++ src/test/java/subway/unit/entity/LineTest.java
@@ -3,17 +3,23 @@ package subway.unit.entity;
 import autoparams.AutoSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import subway.domain.command.LineCommand;
 import subway.domain.entity.line.Line;
+import subway.domain.entity.line.LineSections;
+import subway.domain.exception.SubwayDomainException;
+import subway.domain.exception.SubwayDomainExceptionType;
+import subway.fixtures.LineFixture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LineTest {
+    @DisplayName("init")
     @Nested
     class Init {
-        @DisplayName("init")
         @ParameterizedTest
         @AutoSource
         public void sut_returns_new_line(LineCommand.CreateLine command) {
@@ -34,15 +40,12 @@ public class LineTest {
         }
     }
 
+    @DisplayName("update")
     @Nested
     class Update {
-        @DisplayName("update")
         @ParameterizedTest
         @AutoSource
-        public void sut_updated(
-                Line sut,
-                LineCommand.UpdateLine command
-        ) {
+        public void sut_updated(Line sut, LineCommand.UpdateLine command) {
             // when
             sut.update(command);
 
@@ -50,6 +53,59 @@ public class LineTest {
             // then
             assertThat(sut.getName()).isEqualTo(command.getName());
             assertThat(sut.getColor()).isEqualTo(command.getColor());
+        }
+    }
+
+    @DisplayName("addSection")
+    @Nested
+    class AddSection {
+        @Test
+        public void sut_throws_if_invalid_up_station_section() {
+            // given
+            Line sut = LineFixture.prepareLineOne(1L, 4L);
+
+            // when
+            SubwayDomainException actual = (SubwayDomainException) catchThrowable(() -> sut.addSection(5L, 6L, 10L));
+
+            // then
+            assertThat(actual.getExceptionType()).isEqualTo(SubwayDomainExceptionType.INVALID_UP_STATION);
+        }
+
+        @Test
+        public void sut_throws_if_downStation_already_existed() {
+            // given
+            Line sut = LineFixture.prepareLineOne(1L, 4L);
+
+            // when
+            SubwayDomainException actual = (SubwayDomainException) catchThrowable(() -> sut.addSection(4L, 3L, 10L));
+
+            // then
+            assertThat(actual.getExceptionType()).isEqualTo(SubwayDomainExceptionType.INVALID_DOWN_STATION);
+        }
+
+        @Test
+        public void sut_add_section_if_empty() {
+            // given
+            Line sut = new Line("11", "cc", new LineSections());
+
+            // when
+            sut.addSection(1L, 2L, 10L);
+
+            // then
+            assertThat(sut.getSections().size()).isEqualTo(1);
+        }
+
+        @Test
+        public void sut_add_section_if_lastSection() {
+            // given
+            Line sut = LineFixture.prepareLineOne(1L, 4L);
+
+            // when
+            sut.addSection(4L, 5L, 10L);
+
+            // then
+            assertThat(sut.getSections().getLastSection().getUpStationId()).isEqualTo(4L);
+            assertThat(sut.getSections().getLastSection().getDownStationId()).isEqualTo(5L);
         }
     }
 }
